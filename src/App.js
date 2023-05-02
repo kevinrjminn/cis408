1a8e0edfadd6dae23183cbf6e5fd35bb719bc8e3
gerbrian93 <65586994+gerbrian93@users.noreply.github.com>
2023-05-02 14:53:05 -0400

    added search dates component to find specific data.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Dashboard from './components/Dashboard';
 import { Container, Typography } from '@mui/material';
 import GreenProgressBar from './components/GreenProgressBar';
 import DrawerAppBar from './components/NavBar';
-import DataTable from './components/DataTable'
+import DataTable from './components/DataTable';
+import SearchDates from './components/SearchDates';
 import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TeamInfoPage from './pages/TeamInfoPage';
@@ -28,7 +29,8 @@ function App() {
 
                                     <Container>
 
-                                        <center><br></br><h3>Data Table</h3><DataTable></DataTable></center>
+                                        <center><br></br><h3>Data Table</h3>
+                                        <SearchDates></SearchDates><DataTable></DataTable></center>
                                         <Typography variant="h5" align="center" gutterBottom>
                                             Harvest progress
                                         </Typography>
